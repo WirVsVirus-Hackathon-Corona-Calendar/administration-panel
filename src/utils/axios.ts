9364648435a23319cd8314f814a45e94ca946190
Benjamin Macher <bmacher@Benjamins-MBP.fritz.box>
2020-04-04 15:07:19 +0200

    refactor: switch from axios as plugin to uitls

---
+++ src/utils/axios.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosInstance } from 'axios';
+
+const REDIRECT_URL = 'https://corona-cal.auth.eu-west-1.amazoncognito.com/login?response_type=token&client_id=osupm9rc8kv7jpnhjdtsqu7vj&redirect_uri=https://d30c8b2026z1gr.cloudfront.net';
+
+// #region EXTRACT TOKEN
+let token = '';
+
+if (localStorage.getItem('AUTH_TOKEN')) {
+  token = localStorage.getItem('AUTH_TOKEN') as string;
+} else if (window.location.hash) {
+  // Extract token from URL
+  // Id_token=....
+  [, token] = window.location.hash
+    .split('&')
+    .filter((el) => el.includes('id_token'))[0]
+    // [id_token, <value>]
+    .split('=');
+
+  localStorage.setItem('AUTH_TOKEN', token);
+} else {
+  window.location.href = REDIRECT_URL;
+}
+// #endregion
+
+// TODO: Use environments instead
+const axiosInstance = axios.create({
+  baseURL: 'https://e3bzj7x3ck.execute-api.eu-west-1.amazonaws.com/v1',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+// Redirect to login when 401 occurs
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem('AUTH_TOKEN');
+      window.location.href = REDIRECT_URL;
+
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export {
+  axiosInstance as axios,
+};
