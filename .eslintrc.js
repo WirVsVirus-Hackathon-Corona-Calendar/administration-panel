5df6ea052893d67154f80d423658436cbc082297
Benjamin Macher <bmacher@Benjamins-MBP.fritz.box>
2020-04-06 19:14:25 +0200

    feat(eslint): turn import/prefer-default-export off

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,13 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    project: './tsconfig.json',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': 'off',
+    'import/prefer-default-export': 'off',
   },
   overrides: [
     {
