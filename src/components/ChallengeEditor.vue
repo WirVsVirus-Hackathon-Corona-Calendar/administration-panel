80b625fb21e0d4b97752af3258d3003c260f6705
Benjamin Macher <benjamin.macher@volkswagen.de>
2020-04-08 16:18:29 +0200

    feat(components): add EditableList

--- src/components/ChallengeEditor.vue
+++ src/components/ChallengeEditor.vue
@@ -3,123 +3,179 @@
     ref="form"
     v-model="formIsValid"
   >
-    <v-container ma-0>
+    <v-container class="ma-0 pb-0">
       <v-row>
-        <v-col cols="4">
-          <v-text-field
-            v-model="title"
-            :rules="titleRules"
-            label="Titel"
-            outlined
-            hint="Aussagekräftig, maximal zwei Wörter"
-            required
-          ></v-text-field>
+        <v-col cols=12>
+          <v-card>
+            <!-- TEXT -->
+            <v-container>
+              <v-row>
+                <v-col cols="4" class="pb-0">
+                  <v-text-field
+                    v-model="title"
+                    :rules="titleRules"
+                    label="Titel"
+                    outlined
+                    hint="Aussagekräftig, maximal zwei Wörter"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4" class="pb-0">
+                  <v-textarea
+                    v-model="description"
+                    :rules="[isRequiredRule]"
+                    label="Beschreibung"
+                    textarea
+                    clearable
+                    outlined
+                    required
+                  ></v-textarea>
+                </v-col>
+                <v-col cols="4" class="pb-0">
+                  <v-textarea
+                    v-model="storyBefore"
+                    :rules="[isRequiredRule]"
+                    label="Story davor"
+                    textarea
+                    clearable
+                    outlined
+                    required
+                  ></v-textarea>
+                </v-col>
+                <v-col cols="4" class="pb-0">
+                  <v-textarea
+                    v-model="storyAfter"
+                    :rules="[isRequiredRule]"
+                    label="Story danach"
+                    textarea
+                    clearable
+                    outlined
+                    required
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
         </v-col>
       </v-row>
+    </v-container>
+    <v-container class="ma-0 py-0">
       <v-row>
-        <v-col cols="4" class="pb-0">
-          <v-textarea
-            v-model="description"
-            :rules="[isRequiredRule]"
-            label="Beschreibung"
-            textarea
-            clearable
-            outlined
-            required
-          ></v-textarea>
-        </v-col>
-        <v-col cols="4" class="pb-0">
-          <v-textarea
-            v-model="storyBefore"
-            :rules="[isRequiredRule]"
-            label="Story davor"
-            textarea
-            clearable
-            outlined
-            required
-          ></v-textarea>
-        </v-col>
-        <v-col cols="4" class="pb-0">
-          <v-textarea
-            v-model="storyAfter"
-            :rules="[isRequiredRule]"
-            label="Story danach"
-            textarea
-            clearable
-            outlined
-            required
-          ></v-textarea>
+        <v-col cols=12>
+          <v-card>
+            <!-- IMAGES -->
+            <v-container>
+              <v-row>
+                <v-col cols="4" class="pb-0">
+                  <v-file-input
+                    v-model="icon"
+                    :rules="[isRequiredRule]"
+                    accept="image/*"
+                    label="Hintergrundbild hochladen"
+                    append-icon="attach_file"
+                    prepend-icon=""
+                  ></v-file-input>
+                </v-col>
+                <v-col cols="4" class="pb-0">
+                  <v-file-input
+                    v-model="storyBeforeIcon"
+                    :rules="[isRequiredRule]"
+                    accept="image/*"
+                    label="Bild Story davor hochladen"
+                    append-icon="attach_file"
+                    prepend-icon=""
+                  ></v-file-input>
+                </v-col>
+                <v-col cols="4" class="pb-0">
+                  <v-file-input
+                    v-model="storyAfterIcon"
+                    :rules="[isRequiredRule]"
+                    accept="image/*"
+                    label="Bild Story danach hochladen"
+                    append-icon="attach_file"
+                    prepend-icon=""
+                  ></v-file-input>
+                </v-col>
+              </v-row>
+              <v-row v-if="!!icon || !!storyBeforeIcon || !!storyAfterIcon">
+                <v-col cols="4">
+                  <v-img
+                    :src="iconPreview"
+                    alt="Icon Preview"
+                  />
+                </v-col>
+                <v-col cols="4">
+                  <v-img
+                    :src="storyBeforeIconPreview"
+                    alt="Icon Preview"
+                  />
+                </v-col>
+                <v-col cols="4">
+                  <v-img
+                    :src="storyAfterIconPreview"
+                    alt="Icon Preview"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
         </v-col>
       </v-row>
+    </v-container>
+    <v-container class="ma-0 py-0">
       <v-row>
-        <v-col cols="4" class="pb-0">
-          <v-file-input
-            v-model="icon"
-            :rules="[isRequiredRule]"
-            accept="image/*"
-            label="Hintergrundbild hochladen"
-            append-icon="attach_file"
-            prepend-icon=""
-          ></v-file-input>
-        </v-col>
-        <v-col cols="4" class="pb-0">
-          <v-file-input
-            v-model="storyBeforeIcon"
-            :rules="[isRequiredRule]"
-            accept="image/*"
-            label="Bild Story davor hochladen"
-            append-icon="attach_file"
-            prepend-icon=""
-          ></v-file-input>
-        </v-col>
-        <v-col cols="4" class="pb-0">
-          <v-file-input
-          v-model="storyAfterIcon"
-            :rules="[isRequiredRule]"
-            accept="image/*"
-            label="Bild Story danach hochladen"
-            append-icon="attach_file"
-            prepend-icon=""
-          ></v-file-input>
+        <v-col cols=12>
+          <v-card>
+            <!-- LISTS -->
+            <v-container>
+              <v-row>
+                <v-col cols=4>
+                  <editable-list
+                    v-model="material"
+                    title="Material"
+                  />
+                </v-col>
+                <v-col cols=4>
+                  <editable-list
+                    v-model="instructions"
+                    title="Anleitung"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
         </v-col>
       </v-row>
-      <v-row v-if="!!icon || !!storyBeforeIcon || !!storyAfterIcon">
-        <v-col cols="4">
-          <v-img
-            :src="iconPreview"
-            alt="Icon Preview"
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-img
-            :src="storyBeforeIconPreview"
-            alt="Icon Preview"
-          />
-        </v-col>
-        <v-col cols="4">
-          <v-img
-            :src="storyAfterIconPreview"
-            alt="Icon Preview"
-          />
-        </v-col>
-      </v-row>
+    </v-container>
+    <v-container class="ma-0 py-0">
       <v-row>
-        <v-col>
-          <v-btn
-            :disabled="!formIsValid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-          >
-            Speichern
-          </v-btn>
-          <v-btn
-            color="error"
-            class="mr-4"
-            @click="reset"
-          >
-            Abbrechen
-          </v-btn>
+        <v-col cols=12>
+          <v-card>
+            <!-- CONTROLS -->
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-btn
+                    :disabled="!formIsValid"
+                    color="success"
+                    class="mr-4"
+                    @click="validate"
+                  >
+                    Speichern
+                  </v-btn>
+                  <v-btn
+                    color="error"
+                    class="mr-4"
+                    @click="reset"
+                  >
+                    Abbrechen
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -130,10 +186,9 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
-import { convertImgToB64 } from '@/utils';
+import { convertImgToB64 } from '@/common/utils';
+import { Validator } from '@/common/types';
 import EditableList from './EditableList.vue';
-
-type Validator = (v: string) => boolean | string;
 
 function isRequiredRule(v: string) {
   return !!v || 'Pflichtfeld';
@@ -145,6 +200,7 @@ function isRequiredRule(v: string) {
 })
 export default class ChallengeEditor extends Vue {
   public $refs!: {
+    // FIXME: It is acutally a VueComponent
     form: HTMLFormElement;
   }
 
@@ -170,7 +226,11 @@ export default class ChallengeEditor extends Vue {
 
   @Watch('icon', { immediate: false })
   private async onIconChange(file: File | null): Promise<void> {
-    if (file) this.iconPreview = await convertImgToB64(file);
+    if (file) {
+      this.iconPreview = await convertImgToB64(file);
+    } else {
+      this.iconPreview = '';
+    }
   }
 
   // story before and icon
@@ -182,7 +242,11 @@ export default class ChallengeEditor extends Vue {
 
   @Watch('storyBeforeIcon', { immediate: false })
   private async onStoryBeforeIconChange(file: File | null): Promise<void> {
-    if (file) this.storyBeforeIconPreview = await convertImgToB64(file);
+    if (file) {
+      this.storyBeforeIconPreview = await convertImgToB64(file);
+    } else {
+      this.storyBeforeIconPreview = '';
+    }
   }
 
   // story after and icon
@@ -194,19 +258,30 @@ export default class ChallengeEditor extends Vue {
 
   @Watch('storyAfterIcon', { immediate: false })
   private async onStoryAfterIconChange(file: File | null): Promise<void> {
-    if (file) this.storyAfterIconPreview = await convertImgToB64(file);
+    if (file) {
+      this.storyAfterIconPreview = await convertImgToB64(file);
+    } else {
+      this.storyAfterIconPreview = '';
+    }
   }
+
+  // material
+  public material: string[] = [];
+
+  // instructions
+  public instructions: string[] = [];
   // #endregion
 
   // #region UTILITIES
   public validate() {
     this.$refs.form.validate();
-    }
+  }
 
   public reset() {
     this.$refs.form.reset();
+    this.instructions = [];
+    this.material = [];
   }
   // #endregion
 }
 </script>
-
--- src/components/EditableList.vue
+++ src/components/EditableList.vue
@@ -1,17 +1,74 @@
 <template>
-  <div>
-    <span>Hello from EditableList </span>
-  </div>
+  <v-card flat>
+    <v-card-text class="px-0 pb-1">
+      <p class="body-1 mb-0">{{ title || 'Title' }}</p>
+    </v-card-text>
+    <v-card-text class="px-0">
+      <v-list>
+        <v-subheader class="px-0">
+          <v-text-field
+            ref="input"
+            v-model="item"
+            label="Item hinzufügen"
+            outlined
+            :rules="itemRules"
+            :append-icon="item.length > 0 ? 'add' : ''"
+            :validate-on-blur="true"
+            @click:append="addItem(item)"
+            @keyup.enter="addItem(item)"
+          ></v-text-field>
+        </v-subheader>
+          <v-list-item
+            v-for="(item, index) of items"
+            :key="index"
+            class="pr-0"
+          >
+            <v-list-item-content class="py-0">
+              <v-list-item-title>{{index + 1}}. {{item}}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action class="my-0">
+              <v-btn
+                icon
+                @click.prevent="deleteItem(index)"
+              >
+                <v-icon color="grey lighten-1">delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+import { Validator } from '@/common/types';
 
 @Component({ name: 'EditableList' })
 export default class EditableList extends Vue {
-  @Prop(Array) readonly value: string[] | undefined;
+  public $refs!: {
+    input: HTMLFormElement;
+  }
+
+  @Prop(String) readonly title!: string;
+
+  @Prop(Array) readonly value!: string[];
+
+  public item = '';
+
+  public itemRules: Validator[] = [
+    () => this.items.length > 0 || 'Pflichtfeld',
+  ]
+
+  private get items() {
+    return [...this.value];
+  }
+
+  private set items(items) {
+    this.$emit('input', items);
+  }
 
   public created(): void {
     if (!this.value) {
@@ -23,6 +80,28 @@ export default class EditableList extends Vue {
     if (this.value.length > 0 && foundOtherType) {
       console.error(`EditableList: v-model as to be string[], found ${typeof foundOtherType}.`);
     }
+  }
+
+  public addItem(item: string): void {
+    if (this.item.length > 0) {
+      // this.items = [...this.items, item];
+      this.items = [...this.items, item];
+      this.item = '';
+
+      this.validate();
+    }
+  }
+
+  public deleteItem(index: number): void {
+    this.items = this.items.filter((item, i) => i !== index);
+
+    this.validate();
+  }
+
+  private validate(): void {
+    this.$nextTick(() => {
+      this.$refs.input.validate();
+    });
   }
 }
 </script>
